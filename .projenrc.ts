5f53b62792eacd6e24a4f10a8111f0ef0c252308
Jaap Haitsma <jaap.haitsma@merapar.com>
2025-06-15 12:40:01 -0400

    Use prettier

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,25 +1,26 @@
-import { awscdk } from 'projen';
+import { awscdk } from "projen";
 const project = new awscdk.AwsCdkConstructLibrary({
-  name: 'waf-http-api',
-  packageName: 'waf-http-api',
-  prerelease: 'beta',
+  name: "waf-http-api",
+  packageName: "waf-http-api",
+  prerelease: "beta",
   description:
-    'A CDK construct that fronts an HTTP API with a CloudFront distribution and protects it with AWS WAF.',
-  keywords: ['aws', 'cdk', 'api-gateway', 'http-api', 'waf', 'cloudfront'],
-  author: 'Jaap Haitsma',
-  authorAddress: 'jaap@haitsma.org',
-  copyrightOwner: 'Merapar Technologies Group B.V.',
-  license: 'MIT',
-  cdkVersion: '2.200.0',
-  defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.8.0',
+    "A CDK construct that fronts an HTTP API with a CloudFront distribution and protects it with AWS WAF.",
+  keywords: ["aws", "cdk", "api-gateway", "http-api", "waf", "cloudfront"],
+  author: "Jaap Haitsma",
+  authorAddress: "jaap@haitsma.org",
+  copyrightOwner: "Merapar Technologies Group B.V.",
+  license: "MIT",
+  cdkVersion: "2.200.0",
+  defaultReleaseBranch: "main",
+  jsiiVersion: "~5.8.0",
 
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/merapar/waf-http-api.git',
+  repositoryUrl: "https://github.com/merapar/waf-http-api.git",
 
-  gitignore: ['.vscode'],
+  gitignore: [".vscode"],
 
-  releaseToNpm: true
+  releaseToNpm: true,
+  prettier: true,
 
   // publishToPypi: {
   //   distName: 'merapar.waf-http-api',
